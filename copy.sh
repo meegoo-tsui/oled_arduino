aeac7531b3baf7592660b84a61983a4bc1bdc138
meegoo tsui <meegoo.tsui@gmail.com>
2013-02-07 10:29:58 +0800

    install package for arduino env.

--- copy.sh
+++ copy.sh
@@ -8,6 +8,12 @@
 LOCAL_LIB_PATH=SSD1351OLED
 if [ "$OS" == "Windows_NT" ]; then
 	ARDUINO_LIB_PATH=$HOME/Documents/Arduino/libraries
+else
+	OS=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
+	ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+	VERSION=$(awk '/DISTRIB_RELEASE=/' /etc/*-release | sed 's/DISTRIB_RELEASE=//' | sed 's/[.]0/./')
+	echo "Copy for $OS-$ARCH-$VERSION"
+	ARDUINO_LIB_PATH=$HOME/sketchbook/libraries
 fi
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+ install.sh
+#+   install packages for arduino
+#+ author
+#+   meegoo.tsui@gmail.com, 2013-02-07
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+sudo apt-get install openjdk-6-jre gcc-avr avr-libc
+
