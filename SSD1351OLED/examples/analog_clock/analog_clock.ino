69c4dcf5d5d860c33ab0ed5e63a3368b2c946177
meegoo tsui <meegoo.tsui@gmail.com>
2013-03-02 14:08:56 +0800

    Modify demo to real clock.

--- SSD1351OLED/examples/analog_clock/analog_clock.ino
+++ SSD1351OLED/examples/analog_clock/analog_clock.ino
@@ -20,6 +20,8 @@
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+byte h, m, s;
+
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 /*!
@@ -52,11 +54,14 @@ void setup(void)
 	SSD1351Oled.DrawCircle(64, 64, 20);
 
 	SSD1351Oled.FadeIn();
-	delay(3000);
+	delay(1000);
 	SSD1351Oled.FillRGB(0x00, 0x00, 0x00);
 
 	SSD1351Oled.Printf("Demo - analog clock\n");
 	SSD1351Oled.AnalogClockInit(SSD1351Oled.CenterX,SSD1351Oled.CenterY,SSD1351Oled.CenterY-4);
+	h = 0;
+	m = 0;
+	s = 0;
 }
 
 /******************************************************************************/
@@ -67,12 +72,17 @@ void setup(void)
  */
 void loop(void)
 {
-	byte i;
-
-	i = 0;
 	while(1){
-	    i = i%60;
-		SSD1351Oled.DisplayTime(12, 45, i++);
+		SSD1351Oled.DisplayTime(h, m, s);
+		s++;
+		if(s == 60){
+			s = 0;
+			m++;
+			if(m == 60){
+				m = 0;
+				h = (h + 1)%12;
+			}
+		}
 		delay(1000);
 	}
 }
