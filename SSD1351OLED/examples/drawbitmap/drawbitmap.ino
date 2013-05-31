7c83b276fecca761dd30800a072ae813667f7b3e
meegoo tsui <meegoo.tsui@gmail.com>
2013-05-31 22:59:17 +0800

    No room for 128x128 bmp

--- SSD1351OLED/examples/drawbitmap/drawbitmap.ino
+++ SSD1351OLED/examples/drawbitmap/drawbitmap.ino
@@ -14,7 +14,13 @@
 /* Includes ------------------------------------------------------------------*/
 #include <SSD1351OLED.h>
 #include <SPI.h>
-#include "beauty_64x64.h"
+#define BMP_64_64                      0
+
+#if (BMP_64_64 == 1)
+	#include "beauty_64x64.h"
+#else
+	#include "photo_128x128.h"
+#endif
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -45,8 +51,15 @@ void loop(void)
 
 	while(1){	
 		for(i=0; i<4; i++){
-			SSD1351Oled.Rotate(i);
-			SSD1351Oled.DrawBitmap((uint8_t *)gImage_beauty_64x64, gImage_beauty_64x64_size, 0x20,0x5f,0x20,0x5f);
+			#if (BMP_64_64 == 1)
+				SSD1351Oled.Rotate(i);
+				SSD1351Oled.DrawBitmap((uint8_t *)gImage_beauty_64x64, gImage_beauty_64x64_size, 0x20,0x5f,0x20,0x5f);
+			#else
+				//SSD1351Oled.DrawBitmap((uint8_t *)gImage_photo_128x128_0, gImage_photo_128x32_size, 0x00,0x7f,0x00,0x1f);
+				//SSD1351Oled.DrawBitmap((uint8_t *)gImage_photo_128x128_1, gImage_photo_128x32_size, 0x00,0x7f,0x20,0x3f);
+				SSD1351Oled.DrawBitmap((uint8_t *)gImage_photo_128x128_2, gImage_photo_128x32_size, 0x00,0x7f,0x40,0x5f);
+				SSD1351Oled.DrawBitmap((uint8_t *)gImage_photo_128x128_3, gImage_photo_128x32_size, 0x00,0x7f,0x60,0x7f);
+			#endif
 			SSD1351Oled.FadeIn();
 			delay(2000);
 			SSD1351Oled.FadeOut();
